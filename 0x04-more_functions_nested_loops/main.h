fa9ca0c0717bbd5cc72ae3e667c9ae4be5ccaae3
RASHID JUMA <97727477+RashidJuma198@users.noreply.github.com>
2022-03-17 17:00:02 +0300

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,18 +1,13 @@
 #ifndef MY_HEADER
 #define MY_HEADER
 
-<<<<<<< HEAD
+
 int _isdigit(int c);
 int _isupper(int c);
 int mul(int a, int b);
-=======
-
-int _isdigit(int c);
-int _isupper(int c);
 
 
 
 
->>>>>>> 0a9a8aeb6979dcb54cd0d9f4f3f817e3f7a4670a
 #endif
 
