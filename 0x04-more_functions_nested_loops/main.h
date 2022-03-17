0a9a8aeb6979dcb54cd0d9f4f3f817e3f7a4670a
RASHID JUMA <97727477+RashidJuma198@users.noreply.github.com>
2022-03-17 16:40:02 +0300

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,26 +1,12 @@
 #ifndef MY_HEADER
 #define MY_HEADER
 
-<<<<<<< HEAD
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-void print_to_98(int n);int add(int, int);
-void print_times_table(int n);
-int _isupper(int c)
-int _isdigit(int c)
-=======
+
+int _isdigit(int c);
 int _isupper(int c);
 
 
->>>>>>> 454cacd7748e5b07de7159a47d03fcd081b9719c
+
 
 #endif
 
